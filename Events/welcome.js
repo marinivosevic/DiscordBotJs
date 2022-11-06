e36d957bd93aeee94a9a1187fcccdb8073ac28c9
marinivosevic <95760234+marinivosevic@users.noreply.github.com>
2022-11-06 22:42:37 +0100

    Event handeler i welcome msg

---
+++ Events/welcome.js
@@ -0,0 +1,8 @@
+const welcomeChannelId ="1036669922912247808"
+
+module.exports = {
+    name: 'welcome',
+    execute(member){
+        member.guild.join
+    }
+}
--- bot.js
+++ bot.js
@@ -16,7 +16,22 @@ const client = new Client({
   ],
 });
 
+//*event Handling
 client.commands = new Collection();
+const eventsPath = path.join(__dirname, 'Events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const filePath = path.join(eventsPath, file);
+	const event = require(filePath);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
+}
+
+//*command handaling
 const commandsPath = path.join(__dirname, 'commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 
@@ -25,24 +40,11 @@ for (const file of commandFiles) {
 	const command = require(filePath);
 	client.commands.set(command.data.name, command);
 }
+const welcomeChannelId ="1036669922912247808"
+client.on("guildMemberAdd", (member) =>{
+	member.guild.channels.cache.get(welcomeChannelId).send(`<@${member.id}> Welcome to the server!`)
+})
 
-client.once(Events.ClientReady, () => {
-	console.log('Ready!');
-});
 
-client.on(Events.InteractionCreate, async interaction => {
-	if (!interaction.isChatInputCommand()) return;
-
-	const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
-});
 
 client.login(process.env.TOKEN);
